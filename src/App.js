f0a2fbb50952f533f7ff364f9b36e5c4311984f2
Dev <dparmar1@isi-montreal.com>
2022-06-14 22:20:24 -0400

    Create note ready

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Notes from './components/Notes';
+import Create from './components/Create';
+import { createTheme, ThemeProvider } from '@mui/material'
+import { red } from '@mui/material/colors';
 
 function App() {
+
+  const theme = createTheme({
+    palette: {
+      secondary: red
+    }
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router >
+        <Routes>
+          <Route path="/" element={<Notes />}>
+          </Route>
+          <Route path="/create" element={<Create />}>
+          </Route>
+        </Routes>
+      </Router >
+    </ThemeProvider>
   );
 }
 
